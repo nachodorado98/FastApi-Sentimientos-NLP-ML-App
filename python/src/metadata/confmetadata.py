cf9bb6efcedb9625422ec5c5c4773589f6c00cce
nacho dorado <natxo98@gmail.com>
2023-09-29 14:46:28 +0200

    Creacion de la API

---
+++ python/src/metadata/confmetadata.py
@@ -0,0 +1,17 @@
+TITULO="REST API Detector Sentimientos"
+DESCRIPCION="""
+**REST API Detector sentimientos con FastAPI**
+
+Esta API proporciona un sistema de deteccion de sentimientos de textos.
+
+Los usuarios pueden:
+
+	- Comprobar el sentimiento de un texto introducido
+
+La REST API está diseñada para ofrecer una interfaz sencilla y rápida para gestionar la deteccion de sentimientos.
+
+¡Bienvenido a nuestro sistema de deteccion de sentimientos!
+"""
+VERSION="1.0.0"
+CONTACTO={"name": "Nacho Dorado Ruiz", "email":"natxo98@gmail.com"}
+LICENCIA={"name": "Apache 2.0", "url":"https://www.apache.org/licenses/LICENSE-2.0.html"}
---
+++ python/src/routers/inicio.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, status
+from typing import Dict
+
+from src.modelos.informacion import Informacion
+
+router_inicio=APIRouter(tags=["Inicio"])
+
+
+@router_inicio.get("/", status_code=status.HTTP_200_OK, summary="Devuelve informacion de la API")
+async def inicio()->Informacion:
+
+	"""
+    Devuelve un modelo Informacion con la informacion de la API.
+
+    ## Respuesta
+
+    200 (OK): Si se obtiene el mensaje de informacion correctamente
+
+    - **Mensaje**: El mensaje de bienvenida (str).
+    - **Version**: La version de la API (str).
+    - **Descripcion**: La descripcion de la finalidad de la API (str).
+    - **Documentacion**: La direccion de la documentacion de la API (str).
+    """
+
+	return Informacion(mensaje="¡Bienvenido a la REST API Detector Sentimientos con FastAPI!",
+			         version="1.0.0",
+			         descripcion="""Esta API proporciona un sistema de deteccion de sentimientos de un texto.""",
+			         documentacion="/docs")
